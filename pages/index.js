732a113d002d30b22846d4622c8a1a6ee2037242
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-12 13:01:53 -0400

    created addGuardHandler function to send HTTP request to api route and write to the database. send the function down as a prop to FormModal component

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,29 @@ import FormModal from './components/FormModal';
 export default function Home() {
   const [formOpened, setFormOpened] = useState(false);
 
+  async function addGuardHandler(enteredGuardData) {
+    const res = await fetch('api/new-guard', {
+      method: 'POST',
+      body: JSON.stringify(enteredGuardData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
     <main>
       <button onClick={() => setFormOpened(true)}>Add Guard</button>
 
-      {formOpened && <FormModal setFormOpened={setFormOpened} />}
+      {formOpened && (
+        <FormModal
+          setFormOpened={setFormOpened}
+          addGuardHandler={addGuardHandler}
+        />
+      )}
     </main>
   );
 }
