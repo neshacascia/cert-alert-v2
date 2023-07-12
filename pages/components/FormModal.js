f4e7fac8d5bab89073d657a317deb794ccf8ee14
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-12 11:28:33 -0400

    set up close button for removing FormModal

--- pages/components/FormModal.js
+++ pages/components/FormModal.js
@@ -1,4 +1,7 @@
-export default function FormModal({}) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+
+export default function FormModal({ setFormOpened }) {
   return (
     <div className="bg-[rgba(0,0,0,0.4)] h-screen flex justify-center items-center fixed inset-0 z-50">
       <form
@@ -6,6 +9,12 @@ export default function FormModal({}) {
           'text-black bg-white w-[50%] h-[60%] flex flex-col justify-center items-center'
         }
       >
+        <FontAwesomeIcon
+          icon={faX}
+          onClick={() => setFormOpened(false)}
+          className="font-bold text-lg absolute left-[70%] top-[25%] cursor-pointer"
+        />
+
         <label>First Name</label>
         <input type="text" />
 
