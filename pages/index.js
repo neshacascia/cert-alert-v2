a5cc63cc026b82f93eb9ff1627ac186da91dd7ba
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-12 10:55:06 -0400

    added state for displaying modal when button is clicked

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+
 export default function Home() {
+  const [formOpened, setFormOpened] = useState(false);
+
+  console.log(formOpened);
   return (
     <main>
-      <button>Add Guard</button>
+      <button onClick={() => setFormOpened(true)}>Add Guard</button>
     </main>
   );
 }
