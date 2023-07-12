ad732fe99e5a39199938cde80d72f1b3ce4c9f82
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-12 13:00:34 -0400

    set up handler for adding the guard data to the database

--- pages/api/new-guard.js
+++ pages/api/new-guard.js
@@ -1,1 +1,18 @@
-export default function handler(req, res) {}
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(process.env.NEXT_PUBLIC_API_TOKEN);
+
+    const db = client.db();
+    const guardsCollections = db.collection('guards');
+
+    const result = await guardsCollections.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: 'Guard information inserted!' });
+  }
+}
