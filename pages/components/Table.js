d4018c7ca71d2a1fc8fbd58205ca0778a48f704f
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-13 18:59:03 -0400

    updated table header text colour, font size and font weight

--- pages/components/Table.js
+++ pages/components/Table.js
@@ -3,10 +3,18 @@ export default function Table() {
     <table className="text-left w-full h-auto border-collapse table-auto">
       <thead className="bg-slate-200 h-14">
         <tr>
-          <th className="pl-8">First Name</th>
-          <th className="pl-8">Last Name</th>
-          <th className="pl-8">Security Licence No.</th>
-          <th className="pl-8">Licence Expiry Date</th>
+          <th className="text-slate-700 text-[15px] font-medium pl-8">
+            First Name
+          </th>
+          <th className="text-slate-700 text-[15px] font-medium pl-8">
+            Last Name
+          </th>
+          <th className="text-slate-700 text-[15px] font-medium pl-8">
+            Security Licence No.
+          </th>
+          <th className="text-slate-700 text-[15px] font-medium pl-8">
+            Licence Expiry Date
+          </th>
         </tr>
       </thead>
 
