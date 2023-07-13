5e9e9e0df33c8c9de4435d08fd8c942082bc3138
Nesha Mervin <nesha.mervin@gmail.com>
2023-07-13 19:21:04 -0400

    rounded the outer left and right table header cell top corners

--- pages/components/Table.js
+++ pages/components/Table.js
@@ -3,7 +3,7 @@ export default function Table() {
     <table className="text-left w-full h-auto border-collapse table-auto">
       <thead className="bg-slate-200 h-14 border-b-2 border-slate-400">
         <tr>
-          <th className="text-slate-700 text-[15px] font-medium pl-8">
+          <th className="text-slate-700 text-[15px] font-medium rounded-tl-lg pl-8">
             First Name
           </th>
           <th className="text-slate-700 text-[15px] font-medium pl-8">
@@ -12,7 +12,7 @@ export default function Table() {
           <th className="text-slate-700 text-[15px] font-medium pl-8">
             Security Licence No.
           </th>
-          <th className="text-slate-700 text-[15px] font-medium pl-8">
+          <th className="text-slate-700 text-[15px] font-medium rounded-tr-lg pl-8">
             Licence Expiry Date
           </th>
         </tr>
